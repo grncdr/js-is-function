eb9f24a0fb450c14cd328a03a14eed7a356c3597
Stephen Sugden <me@stephensugden.com>
2014-01-07 12:07:10 -0800

    IE8 and below

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ module.exports = isFunction
 var toString = Object.prototype.toString
 
 function isFunction (fn) {
-  return toString.call(fn) === '[object Function]' || (
-    typeof fn === 'function' && !(fn instanceof RegExp)
-  )
+  return toString.call(fn) === '[object Function]' ||
+    (typeof fn === 'function' && !(fn instanceof RegExp)) ||
+    (typeof window !== 'undefined' &&
+     // IE8 and below
+     (fn === window.setTimeout ||
+      fn === window.alert ||
+      fn === window.confirm ||
+      fn === window.prompt))
 };
