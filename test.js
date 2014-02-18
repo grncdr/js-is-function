5bccc1935090d8a20f45ada77bd35195e938d5cb
Stephen Sugden <glurgle@gmail.com>
2014-02-17 21:57:41 -0800

    First go at a test for #1

--- test.js
+++ test.js
@@ -12,8 +12,25 @@ test('isFunction', function (t) {
     t.ok(!isFunction(42), '42 is not a function')
     t.ok(isFunction(function () {}), 'function is a function')
     t.ok(isFunction(setTimeout), 'setTimeout is a function')
-    if (typeof window !== 'undefined') {
-        t.ok(isFunction(window.alert), 'alert is a function')
-    }
     t.end()
 })
+
+if (typeof window !== 'undefined') {
+    test('browser quirks', function (t) {
+        t.plan(2)
+        
+        t.ok(isFunction(window.alert), 'alert is a function')
+
+        window.testRegExpFromIframe (regexp) {
+            t.ok(!isFunction(regexp))
+        }
+        
+        var iframe = document.createElement('iframe')
+        
+        iframe.contentWindow.document.write([
+            "<html><body><script type=\"text/javascript\">",
+            "parent.testRegExpFromIframe(/a/)"
+            "</script></body></html>"
+        ].join("\n"));
+    })
+}
