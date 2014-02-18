36460c656d9619930c97a4f62869dba5fbc8b338
Stephen Sugden <glurgle@gmail.com>
2014-02-17 21:39:18 -0800

    #1 - regexps can fail instanceof
    This is (apparently) possible if you are testing a RegExp created in an iFrame

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ module.exports = isFunction
 var toString = Object.prototype.toString
 
 function isFunction (fn) {
-  return toString.call(fn) === '[object Function]' ||
-    (typeof fn === 'function' && !(fn instanceof RegExp)) ||
+  var string = toString.call(fn)
+  return string === '[object Function]' ||
+    (typeof fn === 'function' && string !== '[object RegExp]') ||
     (typeof window !== 'undefined' &&
      // IE8 and below
      (fn === window.setTimeout ||
